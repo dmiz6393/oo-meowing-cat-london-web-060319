c563a9d7e81fa2305257a20128800d9942c49654
Danielle Mizrachi <dmiz@brandeis.edu>
2019-06-06 10:09:53 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
 class Cat
     attr_accessor :name 
 
+=======
+## code your solution here. 
+class Cat
+    attr_accessor = :name 
+    
+>>>>>>> 5fe267641522f613ccb17f59ac3cdc1bbee1844a
     def meow
         puts "meow!"
     end 
